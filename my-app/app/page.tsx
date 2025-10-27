ba7950e0a903baab282f02098fd95be04f121ad3
vanishkathakkar <23051151@kiit.ac.in>
2025-10-28 01:27:53 +0530

    02_database

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
-export default function Home() {
+import { db } from "@/lib/db";
   return (
      <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <Button>
