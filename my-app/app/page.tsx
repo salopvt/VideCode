15d1250c5ec9edb80cc1278355245c4197bc79f0
vanishkathakkar <23051151@kiit.ac.in>
2025-10-28 01:03:43 +0530

    added database config

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
-     <div>
+     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <Button>
         Get Started
       </Button>
