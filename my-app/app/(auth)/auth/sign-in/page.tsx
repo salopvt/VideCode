1e8a899fc0b1ef4327fc488ef7563a5cca4b5c7a
vanishkathakkar <23051151@kiit.ac.in>
2025-12-09 16:06:14 +0530

    add sign-in page

--- my-app/app/(auth)/auth/sign-in/page.tsx
+++ my-app/app/(auth)/auth/sign-in/page.tsx
@@ -4,7 +4,7 @@ import SignInFormClient from '@/modules/auth/components/sign-in-form-client'
 const Page = () => {
   return (
     <>
-      <Image src={"/login.svg"} alt='Login-Image' height={300} width={300} className='m-6 object-cover' />
+      <Image src={"/login.svg"} alt='Login-Image' height={500} width={500} className='m-6 object-cover' />
       <SignInFormClient/>
     </>
   )
